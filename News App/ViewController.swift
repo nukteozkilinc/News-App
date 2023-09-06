eceaefa6ea96356d828aa7e4925f33e65c45901e
nukteozkilinc <nukteozkilinc06@gmail.com>
2023-09-06 14:59:26 +0300

    Initial Commit

---
+++ News App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News App
+//
+//  Created by Nukte Ozkilinc on 6.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
